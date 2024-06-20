11c26e6e3f991407c258fed69224dd42b65d0b93
lucasadilla <lucas.pentlandhyde@gmail.com>
2024-06-20 02:07:21 -0400

    updated footer and added pictures

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 let list = document.getElementById('list');
 let ulElement = document.getElementById("list");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cursor = document.createElement('div');
+    cursor.classList.add('cursor');
+    document.body.appendChild(cursor);
+
+    document.addEventListener('mousemove', (e) => {
+        cursor.style.transform = `translate3d(${e.clientX - cursor.offsetWidth / 2}px, ${e.clientY - cursor.offsetHeight / 2}px, 0)`;
+    });
+});
